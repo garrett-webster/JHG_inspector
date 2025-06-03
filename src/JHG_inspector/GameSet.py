3343cb295a29b9ea77428de025e9e824ad8a55ef
Garrett Webster <garrettreeds@gmail.com>
2025-06-02 19:52:09 -0600

    Remove some old code that is no longer needed

--- src/JHG_inspector/GameSet.py
+++ src/JHG_inspector/GameSet.py
@@ -1,4 +1,3 @@
-import re
 from pathlib import Path, PosixPath
 
 from src.JHG_inspector.Game import Game
@@ -21,23 +20,11 @@ class GameSet:
             (name, )
         )
 
-    def get_next_game_id(self):
-        if self.cursor:
-            self.cursor.execute("SELECT seq FROM sqlite_sequence WHERE name = 'games';")
-            row = self.cursor.fetchone()
-            if row is None or row[0] is None:
-                return 1
-            return row[0] + 1
-
     def load_games(self, folder_path, base_path=None):
         game_paths = [f for f in Path(folder_path).iterdir() if f.is_file()]
 
         for game_path in game_paths:
-            try:
-                self.add_game(game_path, base_path=base_path)
-            except FileNotFoundError:
-                print(f"Could not find game {game_path}")
-
+            self.add_game(game_path, base_path=base_path)
 
     def add_game(self, game_path: PosixPath, base_path=None):
         self.games[len(self.games)] = Game(self.connection, game_path, base_path)
