8ae8b8bf1a9e706b849bd13bc3c2887b7c78baff
Garrett Webster <garrettreeds@gmail.com>
2025-06-23 10:42:10 -0600

    Add docstrings and type hints to Container.py

--- src/JHG_inspector/presentation_layer/Container.py
+++ src/JHG_inspector/presentation_layer/Container.py
@@ -1,12 +1,31 @@
+from typing import Callable
+
 from PyQt6.QtCore import Qt, QSize
-from PyQt6.QtWidgets import QSplitter, QTabWidget, QLabel
+from PyQt6.QtWidgets import QSplitter, QTabWidget, QLabel, QWidget
 
 from src.JHG_inspector.presentation_layer.components.ContainerTabBar import ContainerTabBar
 
-
 class Container(QSplitter):
+    """Displays TabbedPanels objects and allows for custom runtime panel layouts.
+
+    Each container is created with a TabbedPanels object by default. It can also have sub-containers added,
+    which creates the custom panel layouts. If all tabs are closed, the TabbedPanels object is deleted. Similarly, if
+    there are no tabs nor sub-containers, the Container object is deleted.
+    """
+
     num_containers = 0
-    def __init__(self, default_panel_check, parent=None):
+    def __init__(self, default_panel_check: Callable, parent=None):
+        """
+        Parameters
+        ----------
+        default_panel_check : Callable
+            A function passed from the CentralPanel that checks if there are any panels left.
+            If there are not, it displays a default pane. Each time a Container object is deleted,
+            it calls default_panel_check.
+        parent : QWidget, optional
+            The QWidget that should be made the parent of the Container.
+        """
+
         super().__init__(parent)
         Container.num_containers += 1
         self.num_containers += 1
@@ -16,9 +35,17 @@ class Container(QSplitter):
         self.tabs = self.TabbedPanels(self.check_is_empty)
         self.addWidget(self.tabs)
 
+    # The orientation determines whether a child container will be displayed horizontally or vertically
     def set_orientation(self, orientation: Qt.Orientation):
+        """
+        Parameters
+        ----------
+        orientation : Qt.Orientation
+            Used to define whether children Containers will be laid out horizontally or vertically.
+        """
         self.orientation = orientation
 
+    # To be empty, a container must have all tabs closed and not have a child container. If that is true, it deletes itself
     def check_is_empty(self):
         if self.tabs.count() == 0 and self.count() == 0:
             self.setParent(None)
@@ -32,7 +59,25 @@ class Container(QSplitter):
             self.setText("New Tab")
 
     class TabbedPanels(QTabWidget):
-        def __init__(self, check_container_empty, parent=None, widget=None):
+        """A tabbed container for panel widgets.
+
+        Allows for panels to occupy the same space. Panels can be dragged from any TabbedPanels object to another.
+        Once the last tab is closed, the TabbedPanels object deletes itself.
+        """
+
+        def __init__(self, check_container_empty: Callable, parent: "Container"=None, widget: QWidget=None):
+            """
+            Parameters
+            ----------
+            check_container_empty : Callable
+                A function passed from the parent Container that checks whether the Container is empty. TabbedPanels
+                calls this function when the last tab is closed or moved to a different TabbedPanels.
+            parent : QWidget, optional
+                The QWidget that should be made the parent of the TabbedPanels.
+            widget : QWidget, optional
+                 The widget (usually a panel associated with a tool) that will be displayed as the first tab when the
+                 TabbedPanels object is added.
+            """
             super().__init__(parent)
             self.deleted = False
             self.check_container_empty = check_container_empty
@@ -72,6 +117,7 @@ class Container(QSplitter):
                 source_tabwidget.deleteLater()
                 source_tabwidget.check_container_empty()
 
+            # Tells the source container to check if it is empty after removing the tab
             source_container = source_tabwidget.parentWidget()
             if source_container and hasattr(source_container, 'check_is_empty'):
                 source_container.check_is_empty()
@@ -82,7 +128,8 @@ class Container(QSplitter):
 
             event.acceptProposedAction()
 
-        def close_tab(self, index):
+        # Closes the current tab. If that was the last tab, removes the TabbedPanels element
+        def close_tab(self, index: int):
             widget = self.widget(index)
             self.removeTab(index)
             if widget is not None:
