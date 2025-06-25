b5fefdbcb0d161162778f83841e75b5aecc69dad
Garrett Webster <garrettreeds@gmail.com>
2025-06-24 18:43:15 -0600

    Add doc strings and type hints to several files.

--- src/JHG_inspector/data_layer/game_file_loaders/GameFileLoader.py
+++ src/JHG_inspector/data_layer/game_file_loaders/GameFileLoader.py
@@ -1,21 +1,27 @@
 import json
 import re
-from pathlib import Path
+from pathlib import Path, PosixPath
 
 from src.JHG_inspector.data_layer.DB_commands.DB_init import TableData
 
-"""Loads the data from the game file in to the table that corresponds with the table_name passed to the load_data decorator
-   The passed function will compile the values for each row in that table. This function gets the table meta data that the
-   decorated function needs to compile the data, then does the insertion operations into the data base.
-   
-   To use a function decorated with load_data, pass a string with the name of the database table you want to insert into
-   load_data (the decoration will look like @load_data("tableName")). The decorated function must take 
-   (self, data, values, table_name) as its arguments. Then, do logic needed to collect the values that should be
-    inserted into the given table and call values.append(), passing it a tuple with the values in the order defined in
-    the schema for that table (see src/data_layer/DB_commands/schema.json)"""
-
 NUM_LOAD_FUNCTIONS = 0
+
+
 def load_data(table_name: str = None):
+    """Decorator that automates getting the schema for the table with the passed table_name and inserting that data.
+
+       Loads the data from the game file in to the table that corresponds with the table_name passed to the load_data
+       decorator The passed function will compile the values for each row in that table. This function gets the table
+       metadata that the decorated function needs to compile the data, then does the insertion operations into the
+       database.
+
+       To use a function decorated with load_data, pass a string with the name of the database table you want to insert
+       into load_data (the decoration will look like @load_data("tableName")). The decorated function must take
+       (self, data, values, table_name) as its arguments. Then, do logic needed to collect the values that should be
+       inserted into the given table and call values.append(), passing it a tuple with the values in the order defined
+       in the schema for that table (see src/data_layer/DB_commands/schema.json)
+       """
+
     def decorator(function):
         global NUM_LOAD_FUNCTIONS
 
@@ -31,6 +37,7 @@ def load_data(table_name: str = None):
                     values
                 )
 
+        # Marks the functions so that they can be collected and run later on
         wrapper._load_function_order = NUM_LOAD_FUNCTIONS
         NUM_LOAD_FUNCTIONS += 1
         return wrapper
@@ -38,15 +45,23 @@ def load_data(table_name: str = None):
     return decorator
 
 
-"""Base class for game file loading. Each subclass defines the functions necessary to load the data from a different 
-   version of the json, allowing for updates to the json to not break existing datasets. These methods (in the subclass)
-   are decorated with @load_data, which marks them to be ran by load_data_from_file. They are run in the order that they
-   are declared, which allows for control of call order for handling dependencies (especially the creation of the 
-   name_to_id and id_to_name dictionaries)"""
-
-
 class GameFileLoader:
-    def __init__(self, game):
+    """Base class for game file loading.
+
+       Each subclass defines the functions necessary to load the data from a different version of the json, allowing
+       updates to the json to not break existing datasets. These methods (in the subclass) are decorated with
+       @load_data, which marks them to be run by load_data_from_file. They are run in the order that they are declared,
+       which allows for control of call order for handling dependencies (especially the creation of the name_to_id and
+       id_to_name dictionaries)
+       """
+
+    def __init__(self, game: "Game"):
+        """
+        Parameters
+        ----------
+        game : JHG_inspector.data_layer.Game
+            A Game object that will hold the data read from the file
+        """
         self.game = game
         self.connection = game.connection
         self.cursor = game.cursor
@@ -64,6 +79,13 @@ class GameFileLoader:
         self._load_functions.sort(key=lambda x: x[0])
 
     def _prepare_sql_strings(self, table_name: str):
+        """Prepares the column name and placeholder strings to be used in insert statements
+
+           Gets the schema of the table with name table_name and creates a string (column_names) that can be used in
+           creating a SQL insert statement. Also creates a string of question marks with the same number of question
+           marks as column names.
+           """
+
         table_data = TableData(self.schema[table_name])
         columns = table_data.non_excluded_columns
         if ('gameId', 'INTEGER') in table_data.columns:
@@ -75,7 +97,15 @@ class GameFileLoader:
 
         return columns, column_names, placeholders
 
-    def load_data_from_file(self, game_path):
+    def load_data_from_file(self, game_path: PosixPath):
+        """Runs all the functions annotated with @load_data, passing in the data loaded from the passed game_path file.
+
+        Parameters
+        ----------
+        game_path: PosixPath
+            The path of the game file to load
+        """
+
         if not game_path.is_file():
             raise FileNotFoundError
 
@@ -83,7 +113,8 @@ class GameFileLoader:
 
         with open(game_path, "r") as game_file:
             data = json.load(game_file)
+
         for _, func in self._load_functions:
-            func(data)  # or func(self, data) depending on your wrapper
+            func(data)
 
         self.connection.commit()
