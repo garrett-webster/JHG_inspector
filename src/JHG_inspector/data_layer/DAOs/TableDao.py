9485fcb29e92a69bbcfa7458b6806e31b56ae015
Garrett Webster <garrettreeds@gmail.com>
2025-06-26 15:54:11 -0600

    Enforce the naming convention on subclasses of TableDao

--- src/JHG_inspector/data_layer/DAOs/TableDao.py
+++ src/JHG_inspector/data_layer/DAOs/TableDao.py
@@ -32,3 +32,20 @@ class TableDoa(ABC):
     @abstractmethod
     def select_all(self):
         ...
+
+    def __init_subclass__(cls, **kwargs):
+        """Ensures that all subclasses follow the TablenameDao naming convention (which the prepare_sql_strings method
+        relies on).
+
+        If the name does not end in 'Dao' or the prefix is not a table found in the schema, then an error is raised.
+        """
+
+        super().__init_subclass__(**kwargs)
+
+        # Enforce naming convention
+        if not cls.__name__.endswith("Dao"):
+            raise TypeError(f"Class name '{cls.__name__}' must end with 'Dao'")
+
+        table_name = cls.__name__[0].lower() + cls.__name__[1:-3]
+        if table_name not in TableDoa.schema:
+            raise ValueError(f"Table name '{table_name}' not found in schema for class '{cls.__name__}'")
