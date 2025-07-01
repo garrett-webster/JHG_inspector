ed1eac6279c893e3b60bacf4c25230ca7f8edcd8
Garrett Webster <garrettreeds@gmail.com>
2025-07-01 10:37:46 -0600

    Add the data layer tests and adapt them for the new data layer.

---
+++ tests/test_data_layer/data_layer_testing_utilities.py
@@ -0,0 +1,63 @@
+import json
+import shutil
+
+import pytest
+
+from src.JHG_inspector.data_layer.Game import Game
+from src.JHG_inspector.logic_layer.DatabaseManager import DatabaseManager
+
+@pytest.fixture
+def temp_folder(tmp_path):
+    test_folder = tmp_path / "test_games"
+    test_folder.mkdir()
+    yield test_folder
+
+@pytest.fixture
+def database_manager(temp_folder):
+    # Make sure JHGInspector creates a new DB at temp_folder
+    return DatabaseManager(temp_folder / "test.db")
+
+@pytest.fixture
+def game_set(temp_folder, database_manager):
+    def create_gameset(path=None, name = "test_set"):
+        database_manager.games.load_games()
+        gameset = database_manager.gamesets.create_gameset(name)
+        if path is not None:
+            if path.exists():
+                for item in path.iterdir():
+                    if item.is_file():
+                        shutil.copy(item, temp_folder / item.name)
+
+            database_manager.games.load_games_from_directory(path)
+            for game_id in database_manager.games.all.keys():
+                gameset.add_game(game_id)
+        return gameset
+    yield create_gameset
+    database_manager.close()
+
+@pytest.fixture
+def game(temp_folder, game_set):
+    def _create_game(path=None):
+
+        # Use default if no data given
+        if path is None:
+            game = Game(game_set().database)
+        else:
+            with open(path, "r") as f:
+                game_data = json.load(f)
+
+            json_path = temp_folder / f"jhg_{game_data['lobby']['code']}.json"
+            with open(json_path, "w") as f:
+                json.dump(game_data, f)
+
+            game = Game(game_set().database)
+            game.load_from_file(json_path)
+        return game
+    return _create_game
+
+@pytest.fixture
+def cursor(temp_folder, database_manager):
+    yield database_manager.connection.cursor()
+
+
+
---
+++ tests/test_data_layer/test_GameSet.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+
+from tests.test_data_layer.data_layer_testing_utilities import *
+
+FILE_PATH = Path(__file__).resolve()
+
+class TestGameSetInitialization:
+    def test_init(self, game_set):
+        assert game_set().name == "test_set"
+
+        cursor = game_set().database.connection.cursor()
+
+        cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name != 'sqlite_sequence';")
+        expected_tables = ["searchTags", "gamesets", "games", "players", "admins", "playersThatWillBeGovernment", "colorGroups",
+            "labelPools", "customParams", "governmentRoundInfo", "customRoundInfo", "transactions", "playerRoundInfo",
+            "influences", "popularities", "groups", "groups_players", "chatInfo", "chatParticipants",
+            "messages", "messageTargets", "gameset_games"]
+        actual_tables = [row[0] for row in cursor.fetchall()]
+
+        assert actual_tables == expected_tables
+
+    def test_load_games_from_database(self, database_manager):
+        database_manager.games.load_games_from_directory(Path(FILE_PATH.parent / "test_set1"))
+        test_gameset = database_manager.gamesets.create_gameset("testing")
+        for game_id in database_manager.games.all.keys():
+            test_gameset.add_game(game_id)
+
+        loaded_games = list(test_gameset.games.values())
+
+        test_gameset.games.clear()
+        test_gameset.load_games()
+
+        assert len(test_gameset.games) == len(loaded_games)
+        for i, game in enumerate(test_gameset.games.values()):
+            assert game.code == loaded_games[i].code
+            assert game.id == loaded_games[i].id
+
+    def test_add_game_from_database(self, game, game_set):
+        game(FILE_PATH.parent / "test_set2/jhg_GDSR.json")
+        test_gameset = game_set(FILE_PATH.parent / "test_set1")
+        test_gameset.add_game(1)
+
+        expected_game_codes = ["GDSR", "MGNP", "GDHP", "PBSG"]
+        expected_game_ids = [1, 2, 3, 4]
+
+        for i, game in enumerate(test_gameset.games.values()):
+            assert game.code == expected_game_codes[i]
+            assert game.id == expected_game_ids[i]
