1c0f0f102845a887adad20bf8e8f0434e84c584a
Garrett Webster <garrettreeds@gmail.com>
2025-07-02 10:27:07 -0600

    Make View a subclass of Panel

--- src/JHG_inspector/presentation_layer/panels/tool_views/View.py
+++ src/JHG_inspector/presentation_layer/panels/tool_views/View.py
@@ -1,6 +1,8 @@
+from src.JHG_inspector.presentation_layer.panels.Panel import Panel
 from src.JHG_inspector.presentation_layer.panels.tool_views.ViewComponents.Component import Component
 
 
-class View:
-    def __init__(self, components: list[Component]):
-        pass
+class View(Panel):
+    def __init__(self):
+        super().__init__()
+        self.components: list[Component] = []
