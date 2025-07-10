84ece6fa962c067ac8ebfcaaa1b0dc97f4738808
Garrett Webster <garrettreeds@gmail.com>
2025-07-10 15:38:53 -0600

    Replace FILE_PATH with TEST_DATA_PATH to simplify the paths used

--- tests/test_logic_layer/test_Game.py
+++ tests/test_logic_layer/test_Game.py
@@ -2,21 +2,21 @@ from pathlib import Path
 
 from tests.testing_utilities import *
 
-FILE_PATH = Path(__file__).resolve()
+TEST_DATA_PATH = Path(__file__).resolve().parent.parent / 'test_data'
 
 
 class TestGameInitialization:
     def test_init_valid_path(self, game):
-        test_game = game(FILE_PATH.parent.parent / "test_data" / "test_set1/jhg_GDHP.json")
+        test_game = game(TEST_DATA_PATH / "test_set1/jhg_GDHP.json")
         assert test_game.code == "GDHP"
 
     def test_init_invalid_path(self, game_set, temp_folder):
         with pytest.raises(FileNotFoundError):
-            Game(game_set().database).load_from_file(Path("../test_data/test_set1/jhg_AAAA.json"))
+            Game(game_set().database).load_from_file(TEST_DATA_PATH / "/test_set1/jhg_AAAA.json")
 
     def test_set_id_to_name_dicts(self, game):
-        test_game1 = game(FILE_PATH.parent.parent / "test_data" / "test_set1/jhg_GDHP.json")
-        test_game2 = game(FILE_PATH.parent.parent / "test_data" / "test_set1/jhg_MGNP.json")
+        test_game1 = game(TEST_DATA_PATH / "test_set1/jhg_GDHP.json")
+        test_game2 = game(TEST_DATA_PATH / "test_set1/jhg_MGNP.json")
 
         expected_game_1 = {1: "Bravo", 2: "Uniform", 3: "X-ray", 4: "Quebec"}
         expected_game_2 = {5: "Sierra", 6: "Romeo", 7: "Uniform", 8: "Tango"}
@@ -25,9 +25,9 @@ class TestGameInitialization:
         assert test_game2.id_to_name == expected_game_2
 
     def test_load_from_file(self, game):
-        test_game1 = game(FILE_PATH.parent.parent / "test_data" / "test_set1/jhg_GDHP.json")
-        test_game2 = game(FILE_PATH.parent.parent / "test_data" / "test_set1/jhg_MGNP.json")
-        test_game3 = game(FILE_PATH.parent.parent / "test_data" / "test_set1/jhg_GDHP.json")
+        test_game1 = game(TEST_DATA_PATH / "test_set1/jhg_GDHP.json")
+        test_game2 = game(TEST_DATA_PATH / "test_set1/jhg_MGNP.json")
+        test_game3 = game(TEST_DATA_PATH / "test_set1/jhg_GDHP.json")
 
         assert test_game1.id == 1
         assert test_game2.id == 2
@@ -38,7 +38,7 @@ class TestGameInitialization:
         assert test_game3.code == "GDHP"
 
     def test_load_from_database(self, game, game_set):
-        game(FILE_PATH.parent.parent / "test_data" / "test_set1/jhg_GDHP.json")
+        game(TEST_DATA_PATH / "test_set1/jhg_GDHP.json")
 
         test_game = game()
         test_game.load_from_database(1)
