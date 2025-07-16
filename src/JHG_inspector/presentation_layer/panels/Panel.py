a5e05003ba777bd26d3de870dedc22d02ca829a7
Garrett Webster <garrettreeds@gmail.com>
2025-07-16 13:40:32 -0600

    Add a default name value

--- src/JHG_inspector/presentation_layer/panels/Panel.py
+++ src/JHG_inspector/presentation_layer/panels/Panel.py
@@ -2,5 +2,6 @@ from PyQt6.QtWidgets import QWidget
 
 
 class Panel(QWidget):
-    def __init__(self, parent=None):
+    def __init__(self, parent=None, name: str = "Untitled"):
         super().__init__(parent)
+        self.name = name
