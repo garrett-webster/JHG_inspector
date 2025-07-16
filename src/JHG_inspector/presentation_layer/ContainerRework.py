cbf5a8cbd87e2ff42896d69f3c181747bb9b926e
Garrett Webster <garrettreeds@gmail.com>
2025-07-16 13:43:33 -0600

    Implement tab splitting

--- src/JHG_inspector/presentation_layer/ContainerRework.py
+++ src/JHG_inspector/presentation_layer/ContainerRework.py
@@ -13,10 +13,12 @@ class Container(QSplitter):
        Each Container can hold a maximum of two items (which is either another Container object or a PanelTabWidget).
        If another item is added, a new container is made and put in that place.
        """
-
+    num_containers = 1
     def __init__(self, item: Union["Container", "PanelTabWidget"], parent_container: "Container" = None,
                  split_direction: Qt.Orientation = Qt.Orientation.Horizontal):
         super().__init__(parent_container)
+        self.container_num = Container.num_containers
+        Container.num_containers += 1
 
         self.parent_container = parent_container
         self.items = [item, None]
@@ -26,7 +28,7 @@ class Container(QSplitter):
         self.setCollapsible(0, False)
 
     def add_item(self, new_item: Union["Container", "PanelTabWidget"],
-                 split_direction: Qt.Orientation = Qt.Orientation.Horizontal, panel: Optional[Panel] = None):
+                 split_direction: Qt.Orientation = Qt.Orientation.Horizontal, existing_item: Optional[Union["Container", "PanelTabWidget"]] = None):
         """Adds a new item to the container.
 
            If there are already two items in the Container, a new Container is created, the panel that was already there
@@ -38,51 +40,50 @@ class Container(QSplitter):
             self.addWidget(new_item)
             self.setOrientation(split_direction)
             self.setCollapsible(1, False)
-        elif panel:
-            new_container = Container(panel, self, split_direction)
-            new_container.add_item(new_item)
-
-            self.replace_item(panel, new_container)
+        elif existing_item:
+            new_container = Container(existing_item, self, split_direction)
+            self.replace_item(existing_item, new_container)
+            new_container.add_item(new_item, split_direction)
+            existing_item.parent_container = new_container
+            new_item.parent_container = new_container
 
         else:
             raise ValueError("Panel must be provided when both items are occupied — the Container needs to know which "
                              "side to split.")
 
-    def remove_item(self, item: Union["Container", "Panel"]):
+    def remove_item(self, item: Union["Container", "PanelTabWidget"]):
         """Removes the item from the container, and removes itself if it is now empty."""
         if item not in self.items:
             raise ValueError("Item not in this Container.")
 
-        self.removeWidget(item)
         item.setParent(None)
         item.deleteLater()
 
         if item == self.items[0]:
+            # If the Container has two elements, promote the 2nd item to the first item. If not, the Container will be
+            # empty following the deletion of the 1st item, and should be removed from the parent Container
             if self.items[1]:
                 promoted = self.items[1]
-                self.removeWidget(promoted)
                 self.items[0] = promoted
                 self.items[1] = None
-                self.addWidget(promoted)
-                self.setCollapsible(0, False)
-            else:
+            elif self.parent_container: # Makes it so that a CentralContainer doesn't call parent_container.remove_item
                 self.parent_container.remove_item(self)
-                return
+            else:
+                self.items[0] = None
         else:
             self.items[1] = None
 
     def replace_item(self, old_item: Union["Container", "Panel"], new_item: Union["Container", "Panel"]):
         """Replaces an item with a new one. Used for both collapsing structures of Containers that only hold other
            Containers and for putting new containers in the place of panels that will be split."""
-        index = self.indexOf(old_item)
+        index = self.items.index(old_item)
         if index == -1:
             raise ValueError("Item not found in this Container.")
 
-        self.removeWidget(old_item)
-        old_item.setParent(None)
-        old_item.deleteLater()
+        old_item.setParent(new_item)
+        new_item.addWidget(old_item)
 
-        self.insertWidget(index, new_item)
+        self.addWidget(new_item)
         self.items[index] = new_item
 
-
+        print("DEBUG")
