9a5ec3e28f9370ae345ab19b2bd011a23320cf27
Garrett Webster <garrettreeds@gmail.com>
2025-08-20 10:12:56 -0600

    Add a bar graph to the round scope for the popularity view.

--- src/JHG_inspector/presentation_layer/GameInspector/PopularityView.py
+++ src/JHG_inspector/presentation_layer/GameInspector/PopularityView.py
@@ -1,24 +1,28 @@
 from typing import Optional, override
 
-from PyQt6.QtWidgets import QComboBox, QWidget, QVBoxLayout, QGridLayout, QLabel
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QWidget, QGridLayout, QLabel, QHBoxLayout, QSizePolicy, QStackedWidget
 
 from src.JHG_inspector.logic_layer.Game import Game
 from src.JHG_inspector.logic_layer.gameset_tools.ToolDataClasses.GraphToolData import GraphToolData
 from src.JHG_inspector.presentation_layer.GameInspector.GameInspectorView import GameInspectorView, update_view_function
 from src.JHG_inspector.presentation_layer.GameInspector.game_inspector_enums import ScopesEnum
+from src.JHG_inspector.presentation_layer.panels.tool_views.ViewComponents.BarGraph import BarGraph
 from src.JHG_inspector.presentation_layer.panels.tool_views.ViewComponents.LinePlotGraph import LinePlotGraph
 
+class TableWidget(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.table_layout = QGridLayout(self)
+        self.setLayout(self.table_layout)
 
-def clear_table(table: QWidget):
-    layout = table.layout()
-
-    while layout.count():
-        item = layout.takeAt(0)
-        widget = item.widget()
-        if widget is not None:
-            widget.setParent(None)
-            widget.deleteLater()
-
+    def clear(self):
+        while self.table_layout.count():
+            item = self.table_layout.takeAt(0)
+            widget = item.widget()
+            if widget is not None:
+                widget.setParent(None)
+                widget.deleteLater()
 
 class PopularityView(GameInspectorView):
     def __init__(self, scope: ScopesEnum, game: Optional[Game] = None, ):
@@ -27,61 +31,72 @@ class PopularityView(GameInspectorView):
         self.scope = scope
 
         self.graph_data = GraphToolData()
-        self.graph = LinePlotGraph(self.graph_data)
+        self.line_graph = LinePlotGraph(self.graph_data)
+        self.bar_graph = BarGraph(self.graph_data)
+        self.graph = QStackedWidget(self)
+        self.graph.addWidget(self.line_graph)
+        self.graph.addWidget(self.bar_graph)
+        self.graph.setCurrentWidget(self.line_graph)
 
-        self.table = QWidget()
-        self.table_layout = QGridLayout(self.table)
+        self.table = TableWidget()
+        self.table.setSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Maximum)
 
-        self.layout.addWidget(self.table)
-        self.layout.addWidget(self.player_selector)
-        self.layout.addWidget(self.graph)
+        self.table_graph_layout = QHBoxLayout()
+        self.table_graph_layout.addWidget(self.table, alignment=Qt.AlignmentFlag.AlignTop)
+        self.table_graph_layout.addWidget(self.graph)
+
+        self.layout.addWidget(self.player_selector, alignment=Qt.AlignmentFlag.AlignTop)
+        self.layout.addWidget(self.round_selector, alignment=Qt.AlignmentFlag.AlignTop)
+        self.layout.addLayout(self.table_graph_layout)
 
         self.update_scope(scope)
-
-    @override
-    def update_scope(self, scope: ScopesEnum):
-        super().update_scope(scope)
-
-    @override
-    def update_player(self, index: int):
-        super().update_player(index)
-        self.update_components()
 
     @update_view_function
     def update_components(self):
         """Updates the data that is used to draw the graph based on the scope selected."""
-        def overview_graph():
+
+        self.table.hide()
+        self.player_selector.hide()
+        self.round_selector.hide()
+
+        def overview_components():
+            self.graph.setCurrentWidget(self.line_graph)
             for player in self.game.players:
                 popularity = player.round_popularity
                 self.graph_data.add_entry(player.name, popularity)
             self.line_graph.update()
 
-        def player_graph():
+        def player_components():
+            self.graph.setCurrentWidget(self.line_graph)
             player = self.selected_player
-            self.graph_data.add_line(player.name, player.round_popularity)
-            self.graph.update()
-            self.graph.show()
             self.graph_data.add_entry(player.name, player.round_popularity)
             self.line_graph.update()
+
+            self.table.clear()
+            self.table.table_layout.addWidget(QLabel("Round"), 0, 0)
+            self.table.table_layout.addWidget(QLabel("Popularity"), 0, 1)
+            for i, popularity in enumerate(player.round_popularity):
+                self.table.table_layout.addWidget(QLabel(str(i+1)), i+1, 0)
+                self.table.table_layout.addWidget(QLabel(str(round(popularity))), i+1, 1)
+
+            self.table.show()
+            self.line_graph.show()
             self.player_selector.show()
 
-            clear_table(self.table)
-            self.table_layout.addWidget(QLabel("Round"), 0, 0)
-            self.table_layout.addWidget(QLabel("Popularity"), 0, 1)
-            for i, popularity in enumerate(player.round_popularity):
-                self.table_layout.addWidget(QLabel(str(i)), i+1, 0)
-                self.table_layout.addWidget(QLabel(str(round(popularity))), i+1, 1)
+        def round_components():
+            self.round_selector.show()
+            self.graph.setCurrentWidget(self.bar_graph)
 
-        def round_graph():
-            self.graph.hide()
-            self.player_selector.hide()
+            for player in self.game.players:
+                self.graph_data.add_entry(player.name, player.round_popularity)
+            self.bar_graph.update(self.selected_round.round_number)
 
-        self.graph_data.clear_lines()
+
         self.graph_data.clear_entries()
         scope_to_function = {
-            ScopesEnum.Overview: overview_graph,
-            ScopesEnum.Player: player_graph,
-            ScopesEnum.Round: round_graph,
+            ScopesEnum.Overview: overview_components,
+            ScopesEnum.Player: player_components,
+            ScopesEnum.Round: round_components,
         }
 
         # Run function to update data associated with the currently selected scope
---
+++ src/JHG_inspector/presentation_layer/panels/tool_views/ViewComponents/BarGraph.py
@@ -0,0 +1,65 @@
+from PyQt6.QtGui import QColor, QFont
+import pyqtgraph as pg
+from pyqtgraph import PlotWidget, BarGraphItem, TextItem
+
+
+class BarGraph(PlotWidget):
+    def __init__(self, data: "GraphToolData"):
+        super().__init__()
+        self.data = data
+
+        self.vb = self.getPlotItem().getViewBox()
+
+        # Hide axis completely (we'll draw our own)
+        self.getPlotItem().showAxis("bottom", False)
+
+        # Fixed baseline at y=0
+        self._baseline = pg.InfiniteLine(
+            angle=0, movable=False,
+            pen=pg.mkPen((150, 150, 150), width=1)
+        )
+        self.addItem(self._baseline)
+
+    def update(self, entry: int = 0):
+        # Clear old bars/labels, keep baseline
+        self.clear()
+        self.addItem(self._baseline)
+
+        # Validate data
+        if not isinstance(self.data.list, list) or not all(isinstance(item, list) for item in self.data.list):
+            raise TypeError("Expected data.to_list() to return a list of lists")
+
+        # Compute max Y for padding
+        all_y = [bar["data"][entry] for bar in self.data.entries if bar["data"]]
+        y_max = max(all_y) if all_y else 1
+        label_margin = -20 # space below baseline for labels
+
+        # Draw bars + labels
+        for i, bar in enumerate(self.data.entries):
+            y_value = bar["data"][entry]
+            label_text = bar["entry_name"]
+            color = QColor(bar["color"])
+
+            # Draw bar
+            self.addItem(BarGraphItem(x=[i], height=[y_value], width=0.5, brush=color))
+
+            # Draw label in data coordinates
+            label_item = pg.TextItem(text=label_text, color="black")
+            label_item.setAnchor((0, 0.5))  # horizontally center, stick bottom
+            label_item.setRotation(270)  # vertical
+            label_item.setPos(i, 0)  # slightly below baseline
+            label_item.setZValue(10)  # ensure it’s on top
+            self.addItem(label_item)
+
+        print(y_max * 1.1)
+        # Set zoom limits: can't zoom out beyond current view, but can zoom in
+        self.vb.setLimits(
+            xMin=-0.5,
+            xMax=len(self.data.entries) - 0.5,
+            yMin=label_margin,
+            yMax=y_max * 1.1
+        )
+
+        # Default zoomed-out view
+        self.vb.setXRange(-0.5, len(self.data.entries) - 0.5)
+        self.vb.setYRange(label_margin, y_max * 1.1)
