622918a5409f2b0e68923b0bdbd388458fdfcc6b
Garrett Webster <garrettreeds@gmail.com>
2025-08-22 15:53:08 -0600

    Fix the selected_player assignment

--- src/JHG_inspector/logic_layer/GameInspector.py
+++ src/JHG_inspector/logic_layer/GameInspector.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 from src.JHG_inspector.logic_layer.Game import Game
+from src.JHG_inspector.logic_layer.Player import Player
 from src.JHG_inspector.logic_layer.Round import Round
 from src.JHG_inspector.presentation_layer.GameInspector.GameInspectorPanel import GameInspectorPanel
 from src.JHG_inspector.presentation_layer.GameInspector.PopularityView import PopularityView
@@ -18,9 +19,7 @@ class GameInspector:
             self.selected_game = games[0]
 
         self.selected_round: Round = self.selected_game.rounds[0]
-
-        if not self.selected_game:
-            self.selected_game = self.games[0]
+        self.selected_player: Player = self.selected_game.players[0]
         # TODO: Sort the games if a gameset has been passed, such that the panel only has access to the games in the
         #  current gameset
 
