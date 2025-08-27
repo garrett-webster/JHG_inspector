9ca48d3e4bb3b1c3b4697b8147715daa5c22dfbf
Garrett Webster <garrettreeds@gmail.com>
2025-08-27 10:42:03 -0600

    Add table description, grid lines, and styling to the table.

--- src/JHG_inspector/presentation_layer/GameInspector/TableWidget.py
+++ src/JHG_inspector/presentation_layer/GameInspector/TableWidget.py
@@ -1,11 +1,37 @@
-from PyQt6.QtWidgets import QGridLayout, QWidget
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QGridLayout, QWidget, QLabel, QVBoxLayout
 
 
 class TableWidget(QWidget):
-    def __init__(self, parent=None):
+    def __init__(self, parent=None, description: str = None, outer_border: bool = False):
         super().__init__(parent)
-        self.table_layout = QGridLayout(self)
-        self.setLayout(self.table_layout)
+        self.setObjectName("TableWidget")
+
+        with open("src/JHG_inspector/presentation_layer/stylesheets/Table.qss", "r") as f:
+            self.setStyleSheet(f.read())
+
+        self.description = QLabel()
+        self.description.setWordWrap(True)
+        self.description.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        if description:
+            self.description.setText(description)
+
+        self.table_body = QWidget()
+        self.table_body.setObjectName("table_body")
+        self.table_body.setAttribute(Qt.WidgetAttribute.WA_StyledBackground, True)
+        self.table_layout = QGridLayout(self.table_body)
+
+        if outer_border:
+            self.table_layout.setContentsMargins(3, 3, 3, 3)
+        else:
+            self.table_layout.setContentsMargins(0, 0, 0, 0)
+
+        self.table_layout.setSpacing(3)
+
+        self.layout = QVBoxLayout(self)
+        self.layout.setSpacing(25)
+        self.layout.addWidget(self.description)
+        self.layout.addWidget(self.table_body)
 
     def clear(self):
         while self.table_layout.count():
@@ -14,3 +40,6 @@ class TableWidget(QWidget):
             if widget is not None:
                 widget.setParent(None)
                 widget.deleteLater()
+
+    def add_widget(self, widget: QWidget, row_num: int, col_num: int):
+        self.table_layout.addWidget(widget, row_num, col_num)
