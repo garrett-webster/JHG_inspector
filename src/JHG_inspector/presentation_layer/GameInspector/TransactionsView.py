952cb0b67aa3744c0ce92e5ca70d8acd9d4356af
Garrett Webster <garrettreeds@gmail.com>
2025-08-27 11:15:32 -0600

    Add table to the transactions overview

--- src/JHG_inspector/presentation_layer/GameInspector/TransactionsView.py
+++ src/JHG_inspector/presentation_layer/GameInspector/TransactionsView.py
@@ -1,28 +1,42 @@
 from typing import override
 
+from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QLabel
 
 from src.JHG_inspector.presentation_layer.GameInspector.GameInspectorView import GameInspectorView
 from src.JHG_inspector.presentation_layer.GameInspector.TableWidget import TableWidget
-from src.JHG_inspector.presentation_layer.GameInspector.game_inspector_enums import ScopesEnum
 
 
 class TransactionsView(GameInspectorView):
     def __init__(self, game_inspector: "GameInspector"):
         super().__init__(game_inspector)
         self.game_inspector = game_inspector
-        self.layout.addWidget(QLabel("Transactions"))
 
-        self.table = TableWidget()
+        self.table = TableWidget(description="Shows the sum of all token allocations from the player listed in the corresponding row to the player in the corresponding column")
+
+        self.layout.addWidget(self.table, alignment=Qt.AlignmentFlag.AlignTop)
 
         self.update_components()
 
-    def update_scope(self, scope: ScopesEnum):
-        print(scope)
-
     @override
     def update_overview_components(self):
-        print(self.game_inspector.selected_game.get_allocations_sum_matrix)
+        game = self.game_inspector.selected_game
+        self.table.clear()
+
+        placeholder_0_0 = QLabel("")
+        placeholder_0_0.setProperty("class", "colRowHeader")
+        self.table.add_widget(placeholder_0_0, 0, 0)
+        for i, player_from_row in enumerate(game.get_allocations_sum_matrix):
+            row_label = QLabel(str(game.id_to_name[game.player_order_to_id[i]]))
+            row_label.setProperty("class", "colRowHeader")
+            col_label = QLabel(str(game.id_to_name[game.player_order_to_id[i]]))
+            col_label.setProperty("class", "colRowHeader")
+
+            self.table.add_widget(row_label, i+1, 0)
+            self.table.add_widget(col_label, 0, i+1)
+            for j, transactions_sum in enumerate(player_from_row):
+                self.table.add_widget(QLabel(str(transactions_sum)), i+1, j+1)
+
 
     @override
     def update_player_components(self):
